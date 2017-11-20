273f19a35dd2dad4982527ba833824f567d6ef12
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-11-20 07:05:08 +0300

    Update heap_sort.h

--- include/heap_sort/heap_sort.h
+++ include/heap_sort/heap_sort.h
@@ -1,32 +1,39 @@
 #include <iostream>
+#include <ctime>
 
-void heapify(int arr[], int n, int i)
+void heapify(int *array, int n, int i)
 {
 	int largest = i;
 	int l = 2 * i + 1;
 	int r = 2 * i + 2;
 
-	if (l < n && arr[l] > arr[largest])
+	if (l < n && array[l] > array[largest])
+	{
 		largest = l;
-
-	if (r < n && arr[r] > arr[largest])
+	}
+	
+	if (r < n && array[r] > array[largest])
+	{
 		largest = r;
-
+	}
+	
 	if (largest != i)
 	{
-		std::swap(arr[i], arr[largest]);
-		heapify(arr, n, largest);
+		std::swap(array[i], array[largest]);
+		heapify(array, n, largest);
 	}
 }
 
-void heap_sort(int arr[], int n)
+void heap_sort(int *array, int n)
 {
 	for (int i = n / 2 - 1; i >= 0; i--)
-		heapify(arr, n, i);
+	{
+		heapify(array, n, i);
+	}
 
 	for (int i = n - 1; i >= 0; i--)
 	{
-		std::swap(arr[0], arr[i]);
-		heapify(arr, i, 0);
+		std::swap(array[0], array[i]);
+		heapify(array, i, 0);
 	}
 }
