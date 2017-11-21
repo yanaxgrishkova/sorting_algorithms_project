ed9c9d5c89c8780720d74d7dbee497a1c1f81ae8
elinagabitova <elinagabitovaa@gmail.com>
2017-11-22 00:00:44 +0500

    Update bubble_sort.h

--- include/bubble_sort/bubble_sort.h
+++ include/bubble_sort/bubble_sort.h
@@ -1,1 +1,10 @@
+#include <iostream>
+template <typename Iterator>
 
+  void bubble_sort (Iterator fisrt, Iterator last)
+{
+    while (first <-- last)
+        for ( Iterator i = first; i < last; ++i )
+            if (*(i + 1) < *i)
+                std::iter_swap( i, i + 1 );
+}
