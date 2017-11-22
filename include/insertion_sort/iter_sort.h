00aa5a8408884b7f6ac4d4a34df8bbbe137fdb22
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-11-22 19:36:26 +0300

    Update iter_sort.h

--- include/insertion_sort/iter_sort.h
+++ include/insertion_sort/iter_sort.h
@@ -7,7 +7,7 @@ void insertion_sort(Iterator first, Iterator last)
 		return;
 	for (Iterator i = first; i != last; ++i)
 	{
-		for (Iterator j = i - 1; (j != first) && (*j < *(j - 1)); --j)
+		for (Iterator j = i; (j != first) && (*j < *(j - 1)); --j)
 		{
 			std::iter_swap((j - 1), j);
 		}
