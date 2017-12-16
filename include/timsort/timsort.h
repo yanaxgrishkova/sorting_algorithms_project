fdc4bc761a2555c2523d4fef4d1bb472a768a3ff
elinagabitova <elinagabitovaa@gmail.com>
2017-12-16 22:03:01 +0300

    Update timsort.h

--- include/timsort/timsort.h
+++ include/timsort/timsort.h
@@ -2,78 +2,8 @@
 #include <vector>
 #include <algorithm>
 
-template <typename Iterator>
-void insertion_sort(Iterator first, Iterator last)
-{
-	if (first >= last)
-	{
-		return;
-	}
-	for (Iterator i = first; i != last; ++i)
-	{
-		for (Iterator j = i; (j != first) && (*j < *(j - 1)); --j)
-		{
-			std::iter_swap((j - 1), j);
-		}
-	}
-}
-
-	template <typename T>
-	void merge(T *array, T *buffer, int left, int middle, int right)
-	{
-		int _left = 0;
-		int _right = 0;
-
-		while (left + _left < middle && middle + _right < right)
-		{
-			if (less(array[left + _left], array[middle + _right]))
-			{
-				buffer[_left + _right] = array[left + _left];
-				_left++;
-			}
-			else
-			{
-				buffer[_left + _right] = array[middle + _right];
-				_right++;
-			}
-		}
-
-		while (left + _left < middle)
-		{
-			buffer[_left + _right] = array[left + _left];
-			_left++;
-		}
-
-		while (middle + _right < right)
-		{
-			buffer[_left + _right] = array[middle + _right];
-			_right++;
-		}
-
-		for (int i = 0; i < _left + _right; i++)
-		{
-			array[left + i] = buffer[i];
-		}
-	}
-
-
-template <typename T>
-void merge_sort(T *array, T *buffer, int left, int right)
-{
-	if (right - left <= 1)
-	{
-		return;
-	}
-
-	int middle = left + (right - left) / 2;
-
-	merge_sort(array, buffer, left, middle);
-	merge_sort(array, buffer, middle, right);
-
-	merge(array, buffer, left, middle, right);
-}
-
-
+#include <insertion_sort.h>
+#include <merge_sort.h>
 
 template <typename Iterator>
 void tim_sort(Iterator begin, Iterator end, int N)
