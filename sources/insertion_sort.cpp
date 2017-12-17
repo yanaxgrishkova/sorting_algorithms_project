7fc62cc6cbf279d877459d2382473f65f103339c
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-12-17 22:31:07 +0300

    Update insertion_sort.cpp

--- sources/insertion_sort.cpp
+++ sources/insertion_sort.cpp
@@ -1,20 +1,6 @@
-#include "insertion_sort.h"
-
-void print_array(int array[], int size)
-{
-	for (int j = 0; j < size; j++)
-	{
-		std::cout << array[j] << " ";
-	}
-}
-
-
-int main() {
-	int array[] = { 4, 6, 3, 7, 5, 9, 2, 8, 1, 10 };
-	const int size = sizeof(array) / sizeof(array[0]);
-	insertion_sort(array, size);
-	print_array(array, size);
-	system("pause");
-	return 0;
-}
-
+#include "sorting_algorithms_project/include/quick_sort/quick_sort_iter.h"
+#include "sorting_algorithms_project/include/select_sort/selection_sort.h"
+#include "sorting_algorithms_project/include/merge_sort/merge_sort_iter.h"
+#include "sorting_algorithms_project/include/insertion_sort/itertion_sort.h"
+#include "sorting_algorithms_project/include/counting_sort/counting_sort.h"
+#include "sorting_algorithms_project/include/bubble_sort/bubble_sort.h"
