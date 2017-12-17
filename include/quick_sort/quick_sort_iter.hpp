f30ea8339b203d57ef2cff0f58b4fc06d8656257
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-12-17 19:42:27 +0300

    Update quick_sort_iter.hpp

--- include/quick_sort/quick_sort_iter.hpp
+++ include/quick_sort/quick_sort_iter.hpp
@@ -4,4 +4,31 @@
 #include <iterator>
 #include <typeinfo>  
 
-http://fsgn.bmstu.ru/GSG1.jpg
+template <typename Iterator>
+void quick_sort(Iterator first, Iterator last)
+{
+	if (first < last)
+	{
+		p = partition(first, last);
+		quickSort(first, p - 1);
+		quickSort(p + 1, last);
+	}
+}
+
+template <typename Iterator>
+void partition(Iterator first, Iterator last)
+{
+	auto pivot = *last;
+	Iterator i = first - 1;
+	for (Iterator j = first; j < last - 1; ++j)
+	{
+		if (*j <= pivot)
+		{
+			i++;
+			std::swap(*i, *j)
+		}
+	}
+	std::swap(*(i + 1), *last);
+	
+	return i + 1;
+}
