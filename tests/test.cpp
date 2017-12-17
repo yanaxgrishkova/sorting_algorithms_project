c83efdd29d29205dfd5a1babb73e37d22cd42e96
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-12-17 22:57:04 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,3 +1,13 @@
 #include <catch.hpp>
 
-#include "bubble_sort/bubble_sort.h"
+#include "sorting_algorithms_project/include/quick_sort/quick_sort_iter.h"
+
+TEST_CASE("QUICK SORT", "") 
+{
+  int arr[] = {0};
+  int* first = &arr[0];
+  int* last = &arr[0];
+  quick_sort(first, last);
+  REQUIRE(arr[0] == 0);
+}
+  
