80b2e092d71705b5648a2c5d17d04469696e5b09
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-12-17 20:14:32 +0300

    Update merge_sort_iter.h

--- include/merge_sort/merge_sort_iter.h
+++ include/merge_sort/merge_sort_iter.h
@@ -1,53 +1,34 @@
 #pragma once
 
 #include <algorithm> 
-#include <vector>
 
-namespace sortings
+template <typename Iterator>
+void sort(Iterator first, Iterator last, Iterator firstCopy, long lo, long hi, Compare& comp )
 {
-	class MergeSort
-	{
-	public:
-		template< class RandomAccessIterator >
-		static void sort( RandomAccessIterator first, RandomAccessIterator last )
-		{
-			sort( first, last, std::less< std::iterator_traits<RandomAccessIterator>::value_type >() );
-		}
+	if( hi <= lo ) 
+		return;
+	long mid = lo + ( hi - lo ) / 2;
+	sort( first, last, firstCopy, lo, mid, comp );
+	sort( first, last, firstCopy, mid + 1, hi, comp );
+	merge( first, last, firstCopy, lo, mid, hi, comp );
+}
 
-	private:
-		template< class RandomAccessIterator, class Compare >
-		static void sort( RandomAccessIterator first, RandomAccessIterator last, RandomAccessIterator firstCopy, long lo, long hi, Compare& comp )
-		{
-			if( hi <= lo ) 
-				return;
-			long mid = lo + ( hi - lo ) / 2;
-			sort( first, last, firstCopy, lo, mid, comp );
-			sort( first, last, firstCopy, mid + 1, hi, comp );
-			merge( first, last, firstCopy, lo, mid, hi, comp );
-		}
+template <typename Iterator>
+void merge(Iterator first, Iterator last, Iterator firstCopy, long lo, long mid, long hi, Compare& comp)
+{
+	std::copy( first + lo, first + hi, firstCopy + lo );
 
-		template< class RandomAccessIterator, class Compare >
-		static void merge( RandomAccessIterator first, RandomAccessIterator last, RandomAccessIterator firstCopy, long lo, long mid, long hi, Compare& comp)
-		{
-			std::copy( first + lo, first + hi, firstCopy + lo );
-
-			auto i = lo, j = mid + 1;
+	auto i = lo, j = mid + 1;
       
-			for( auto k = lo; k != (hi + 1); k++ )
-			{
-				if	( i > mid )								
-					first[k] = firstCopy[j++];
-				else if	( j > hi )								
-					first[k] = firstCopy[i++];
-				else if ( comp( firstCopy[j], firstCopy[i] ) )	
-					first[k] = firstCopy[j++];
-				else											
-					first[k] = firstCopy[i++];
-			}
-		}
-
-	private:
-		MergeSort();
-		~MergeSort();
-	};
+	for( auto k = lo; k != (hi + 1); k++ )
+	{
+		if	( i > mid )								
+			first[k] = firstCopy[j++];
+		else if	( j > hi )								
+			first[k] = firstCopy[i++];
+		else if ( comp( firstCopy[j], firstCopy[i] ) )	
+			first[k] = firstCopy[j++];
+		else											
+			first[k] = firstCopy[i++];
+	}
 }
