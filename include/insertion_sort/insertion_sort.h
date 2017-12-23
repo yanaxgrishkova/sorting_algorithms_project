14bfe3217321763f74bafcacf84590200cd82aac
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-12-23 15:31:31 +0300

    Update insertion_sort.h

--- include/insertion_sort/insertion_sort.h
+++ include/insertion_sort/insertion_sort.h
@@ -1,16 +1,16 @@
-#pragma once
-
 #include <algorithm>
 
-template typename Iterator>
-void sort (Iterator first, Iterator last)
+template <typename Iterator>
+void insertion_sort(Iterator first, Iterator last)
 {
 	if (first == last)
 		return;
+	
 	for (auto i = first; i != last; ++i)
 	{
 		for (auto j = i; (j != first) && (*j < *(j - 1)); --j)
-		{				
-				std::iter_swap((j - 1), j);
-		}	
+		{
+			std::iter_swap((j - 1), j);
+		}
+	}
 }
