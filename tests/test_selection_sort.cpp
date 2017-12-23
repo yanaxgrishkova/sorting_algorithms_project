05d0d6e688d529613d98dc406b2a7be18fe4d11b
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-12-23 22:52:49 +0300

    Update test_selection_sort.cpp

--- tests/test_selection_sort.cpp
+++ tests/test_selection_sort.cpp
@@ -4,6 +4,8 @@
 
 #include "selection_sort.h"
 
+//////////////////////////////////// Сортировка рандомных массивов длины 1, 10, 100, 500 и 1000 ////////////////////////////////////
+
 TEST_CASE("selestion_sort", "size = 1") 
 {
 	srand(time(NULL));
@@ -101,6 +103,96 @@ TEST_CASE("selestion_sort_1000", "size = 1000")
 	for (int i = 0; i < size; i++)
   	{
 		arr[i] = rand() % 1000;
+  	}
+  
+	selection_sort(&arr[0], &arr[size]);
+  
+	std::vector<int> v(arr, arr + size);
+	std::sort(v.begin(), v.end());
+  
+	for (int i = 0; i < size; i++) 
+	{
+		REQUIRE(arr[i] == v[i]);
+	}
+}
+
+//////////////////////////////////// Сортировка обратных массивов длины 10, 100, 500 и 1000 ////////////////////////////////////
+
+TEST_CASE("selestion_sort_1_10, "size = 10") 
+{
+	srand(time(NULL));
+	const int size = 10;
+	int arr[size];
+  
+	for (int i = 0; i < size; i++)
+  	{
+		arr[i] = size - i;
+  	}
+  
+	selection_sort(&arr[0], &arr[size]);
+  
+	std::vector<int> v(arr, arr + size);
+	std::sort(v.begin(), v.end());
+  
+	for (int i = 0; i < size; i++) 
+	{
+		REQUIRE(arr[i] == v[i]);
+	}
+}
+
+TEST_CASE("selestion_sort_1_100, "size = 100") 
+{
+	srand(time(NULL));
+	const int size = 100;
+	int arr[size];
+  
+	for (int i = 0; i < size; i++)
+  	{
+		arr[i] = size - i;
+  	}
+  
+	selection_sort(&arr[0], &arr[size]);
+  
+	std::vector<int> v(arr, arr + size);
+	std::sort(v.begin(), v.end());
+  
+	for (int i = 0; i < size; i++) 
+	{
+		REQUIRE(arr[i] == v[i]);
+	}
+}
+
+TEST_CASE("selestion_sort_1_500, "size = 500") 
+{
+	srand(time(NULL));
+	const int size = 500;
+	int arr[size];
+  
+	for (int i = 0; i < size; i++)
+  	{
+		arr[i] = size - i;
+  	}
+  
+	selection_sort(&arr[0], &arr[size]);
+  
+	std::vector<int> v(arr, arr + size);
+	std::sort(v.begin(), v.end());
+  
+	for (int i = 0; i < size; i++) 
+	{
+		REQUIRE(arr[i] == v[i]);
+	}
+}
+	  
+TEST_CASE("selestion_sort_1_1000, "size = 1000") 
+{
+	srand(time(NULL));
+	const int size = 1000;
+	int arr[size];
+  
+	for (int i = 0; i < size; i++)
+  	{
+		arr[i] = size - i;
   	}
   
 	selection_sort(&arr[0], &arr[size]);
