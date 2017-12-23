76b7ca60f7e0eb2787675603d4d5ab71bc7ffdbb
Elina Gabitova <elinagabitovaa@gmail.com>
2017-12-23 23:20:35 +0300

    Update test_bubble_sort.cpp

--- tests/test_bubble_sort.cpp
+++ tests/test_bubble_sort.cpp
@@ -224,5 +224,115 @@ std::sort(sorted.begin(), sorted.end());
   }
 }
 
+TEST_CASE("bubble_sort_fewunique", "N=1") 
+{
+ srand(time(NULL));
+ const int N = 1;
+ 
+ int nosort[N];
+ for (int i = 0; i<N; i++)
+ {
+ nosort[i] = rand() % 3 ;
+ }
+ 
+ bubble_sort(&nosort[0], &nosort[N]);
+
+std::vector<int> sorted(nosort, nosort + N);
+std::sort(sorted.begin(), sorted.end());
+  
+  for (int i = 0; i < N; i++) 
+  {
+        REQUIRE(nosort[i] == sorted[i]);
+  }
+}
+
+TEST_CASE("bubble_sort_fewunique_1", "N=10") 
+{
+ srand(time(NULL));
+ const int N = 10;
+ 
+ int nosort[N];
+ for (int i = 0; i<N; i++)
+ {
+ nosort[i] = rand() % 3 ;
+ }
+ 
+ bubble_sort(&nosort[0], &nosort[N]);
+
+std::vector<int> sorted(nosort, nosort + N);
+std::sort(sorted.begin(), sorted.end());
+  
+  for (int i = 0; i < N; i++) 
+  {
+        REQUIRE(nosort[i] == sorted[i]);
+  }
+}
 
 
+TEST_CASE("bubble_sort_fewunique_2", "N=100") 
+{
+ srand(time(NULL));
+ const int N = 100;
+ 
+ int nosort[N];
+ for (int i = 0; i<N; i++)
+ {
+ nosort[i] = rand() % 3 ;
+ }
+ 
+ bubble_sort(&nosort[0], &nosort[N]);
+
+std::vector<int> sorted(nosort, nosort + N);
+std::sort(sorted.begin(), sorted.end());
+  
+  for (int i = 0; i < N; i++) 
+  {
+        REQUIRE(nosort[i] == sorted[i]);
+  }
+}
+
+
+TEST_CASE("bubble_sort_fewunique_3", "N=500") 
+{
+ srand(time(NULL));
+ const int N = 500;
+ 
+ int nosort[N];
+ for (int i = 0; i<N; i++)
+ {
+ nosort[i] = rand() % 3 ;
+ }
+ 
+ bubble_sort(&nosort[0], &nosort[N]);
+
+std::vector<int> sorted(nosort, nosort + N);
+std::sort(sorted.begin(), sorted.end());
+  
+  for (int i = 0; i < N; i++) 
+  {
+        REQUIRE(nosort[i] == sorted[i]);
+  }
+}
+
+
+TEST_CASE("bubble_sort_fewunique_4", "N=1000") 
+{
+ srand(time(NULL));
+ const int N = 1000;
+ 
+ int nosort[N];
+ for (int i = 0; i<N; i++)
+ {
+ nosort[i] = rand() % 3 ;
+ }
+ 
+ bubble_sort(&nosort[0], &nosort[N]);
+
+std::vector<int> sorted(nosort, nosort + N);
+std::sort(sorted.begin(), sorted.end());
+  
+  for (int i = 0; i < N; i++) 
+  {
+        REQUIRE(nosort[i] == sorted[i]);
+  }
+}
