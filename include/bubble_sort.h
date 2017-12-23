d1d74a3c5894d695ee696e8be7198b25bb48af4b
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-12-23 15:29:04 +0300

    Update and rename include/bubble_sort/bubble_sort.h to include/bubble_sort.h

---
+++ include/bubble_sort.h
@@ -0,0 +1,10 @@
+#include <iostream>
+
+template <typename Iterator>
+void bubble_sort(Iterator first, Iterator last)
+{
+    while (first <-- last)
+        for (Iterator i = first; i < last; ++i)
+            if (*(i + 1) < *i)
+                std::iter_swap(i, i + 1);
+}
