fb1d98f1c0dcb0465ceb058deb69432db97cc313
Elina Gabitova <elinagabitovaa@gmail.com>
2017-12-23 22:18:23 +0300

    Update test_bubble_sort.cpp

--- tests/test_bubble_sort.cpp
+++ tests/test_bubble_sort.cpp
@@ -2,10 +2,101 @@
 
 #include "bubble_sort.h"
 
-TEST_CASE("bubble_sort", "") 
+TEST_CASE("bubble_sort", "N=1") 
+{
+ srand(time(NULL));
+ const int N = 1;
+ 
+ int nosort[N];
+ for (int i = 0; i<N; i++)
+ nosort[i] = rand () % 1000;
+ 
+ 
+ bubble_sort(&nosort[0], &nosort[N]);
+
+std::vector<int> sorted(nosort, nosort + N);
+std::sort(sorted.begin(), sorted.end());
+  
+  for (int i = 0; i < N; i++) 
+  {
+        REQUIRE(nosort[i] == sorted[i]);
+  }
+}
+
+
+TEST_CASE("bubble_sort", "N=10") 
 {
  srand(time(NULL));
  const int N = 10;
+ 
+ int nosort[N];
+ for (int i = 0; i<N; i++)
+ {
+ nosort[i] = rand () % 1000;
+ }
+ 
+ bubble_sort(&nosort[0], &nosort[N]);
+
+std::vector<int> sorted(nosort, nosort + N);
+std::sort(sorted.begin(), sorted.end());
+  
+  for (int i = 0; i < N; i++) 
+  {
+        REQUIRE(nosort[i] == sorted[i]);
+  }
+}
+
+
+TEST_CASE("bubble_sort", "N = 100") 
+{
+ srand(time(NULL));
+ const int N = 100;
+ 
+ int nosort[N];
+ for (int i = 0; i<N; i++)
+ {
+ nosort[i] = rand () % 1000;
+ }
+ 
+ bubble_sort(&nosort[0], &nosort[N]);
+
+std::vector<int> sorted(nosort, nosort + N);
+std::sort(sorted.begin(), sorted.end());
+  
+  for (int i = 0; i < N; i++) 
+  {
+        REQUIRE(nosort[i] == sorted[i]);
+  }
+}
+
+
+TEST_CASE("bubble_sort", "N=500") 
+{
+ srand(time(NULL));
+ const int N = 500;
+ 
+ int nosort[N];
+ for (int i = 0; i<N; i++)
+ {
+ nosort[i] = rand () % 1000;
+ }
+ 
+ bubble_sort(&nosort[0], &nosort[N]);
+
+std::vector<int> sorted(nosort, nosort + N);
+std::sort(sorted.begin(), sorted.end());
+  
+  for (int i = 0; i < N; i++) 
+  {
+        REQUIRE(nosort[i] == sorted[i]);
+  }
+}
+
+
+TEST_CASE("bubble_sort", "N=1000") 
+{
+ srand(time(NULL));
+ const int N = 1000;
  
  int nosort[N];
  for (int i = 0; i<N; i++)
