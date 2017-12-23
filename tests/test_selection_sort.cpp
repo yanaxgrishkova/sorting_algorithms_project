4cf467eefa802f8e3e18f1c576ec751555415882
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-12-23 23:35:11 +0300

    Update test_selection_sort.cpp

--- tests/test_selection_sort.cpp
+++ tests/test_selection_sort.cpp
@@ -205,3 +205,73 @@ TEST_CASE("selestion_sort_1_1000")
 		REQUIRE(arr[i] == v[i]);
 	}
 }
+
+
+//////////////////////////////////// Сортировка обратных массивов, заполненных значениями в промежутке [0; 3] ////////////////////////////////////
+
+TEST_CASE("selestion_sort_2_10") 
+{
+	srand(time(NULL));
+	const int size = 10;
+	int arr[size];
+  
+	for (int i = 0; i < size; i++)
+  	{
+		arr[i] = rand() % 3;
+  	}
+  
+	selection_sort(&arr[0], &arr[size]);
+  
+	std::vector<int> v(arr, arr + size);
+	std::sort(v.begin(), v.end());
+  
+	for (int i = 0; i < size; i++) 
+	{
+		REQUIRE(arr[i] == v[i]);
+	}
+}
+
+TEST_CASE("selestion_sort_1_100") 
+{
+	srand(time(NULL));
+	const int size = 100;
+	int arr[size];
+  
+	for (int i = 0; i < size; i++)
+  	{
+		arr[i] = rand() % 3;
+  	}
+  
+	selection_sort(&arr[0], &arr[size]);
+  
+	std::vector<int> v(arr, arr + size);
+	std::sort(v.begin(), v.end());
+  
+	for (int i = 0; i < size; i++) 
+	{
+		REQUIRE(arr[i] == v[i]);
+	}
+}
+
+
+TEST_CASE("selestion_sort_1_100") 
+{
+	srand(time(NULL));
+	const int size = 1000;
+	int arr[size];
+  
+	for (int i = 0; i < size; i++)
+  	{
+		arr[i] = rand() % 3;
+  	}
+  
+	selection_sort(&arr[0], &arr[size]);
+  
+	std::vector<int> v(arr, arr + size);
+	std::sort(v.begin(), v.end());
+  
+	for (int i = 0; i < size; i++) 
+	{
+		REQUIRE(arr[i] == v[i]);
+	}
+}
