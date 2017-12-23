1369471c5b0ced6416a8c9e26d5d26784ece63ab
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-12-23 22:17:28 +0300

    Update test_selection_sort.cpp

--- tests/test_selection_sort.cpp
+++ tests/test_selection_sort.cpp
@@ -1,17 +1,107 @@
 #include <catch.hpp>
+#include <ctime> 
+#include <utility>
 
 #include "selection_sort.h"
 
-TEST_CASE("selestion_sort", "") 
+TEST_CASE("selestion_sort", "size = 1") 
 {
 	srand(time(NULL));
 	const int size = 1;
 	int arr[size];
   
 	for (int i = 0; i < size; i++)
-  {
+  	{
 		arr[i] = rand() % 1000;
+  	}
+  
+	selection_sort(&arr[0], &arr[size]);
+  
+  std::vector<int> v(arr, arr + size);
+  std::sort(v.begin(), v.end());
+  
+  for (int i = 0; i < size; i++) 
+  {
+        REQUIRE(arr[i] == v[i]);
   }
+}
+
+TEST_CASE("selestion_sort", "size = 10") 
+{
+	srand(time(NULL));
+	const int size = 10;
+	int arr[size];
+  
+	for (int i = 0; i < size; i++)
+  	{
+		arr[i] = rand() % 1000;
+  	}
+  
+	selection_sort(&arr[0], &arr[size]);
+  
+  std::vector<int> v(arr, arr + size);
+  std::sort(v.begin(), v.end());
+  
+  for (int i = 0; i < size; i++) 
+  {
+        REQUIRE(arr[i] == v[i]);
+  }
+}
+
+TEST_CASE("selestion_sort", "size = 100") 
+{
+	srand(time(NULL));
+	const int size = 100;
+	int arr[size];
+  
+	for (int i = 0; i < size; i++)
+  	{
+		arr[i] = rand() % 1000;
+  	}
+  
+	selection_sort(&arr[0], &arr[size]);
+  
+  std::vector<int> v(arr, arr + size);
+  std::sort(v.begin(), v.end());
+  
+  for (int i = 0; i < size; i++) 
+  {
+        REQUIRE(arr[i] == v[i]);
+  }
+}
+
+TEST_CASE("selestion_sort", "size = 500") 
+{
+	srand(time(NULL));
+	const int size = 500;
+	int arr[size];
+  
+	for (int i = 0; i < size; i++)
+  	{
+		arr[i] = rand() % 1000;
+  	}
+  
+	selection_sort(&arr[0], &arr[size]);
+  
+  std::vector<int> v(arr, arr + size);
+  std::sort(v.begin(), v.end());
+  
+  for (int i = 0; i < size; i++) 
+  {
+        REQUIRE(arr[i] == v[i]);
+  }
+}
+
+TEST_CASE("selestion_sort", "size = 1000") 
+{
+	srand(time(NULL));
+	const int size = 1000;
+	int arr[size];
+  
+	for (int i = 0; i < size; i++)
+  	{
+		arr[i] = rand() % 1000;
+  	}
   
 	selection_sort(&arr[0], &arr[size]);
   
