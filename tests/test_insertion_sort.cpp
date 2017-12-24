579d41830bc9b5dc2a9fbe277d89e0550c82fb8b
Elina Gabitova <elinagabitovaa@gmail.com>
2017-12-24 16:11:30 +0300

    Create test_insertion_sort.cpp

---
+++ tests/test_insertion_sort.cpp
@@ -0,0 +1,324 @@
+#include "insertion_sort.h"
+
+//////////////////////////////////// Сортировка рандомных массивов длины 1, 10, 100, 500 и 1000 ////////////////////////////////////
+
+TEST_CASE("insertion_sort", "N=1")
+{
+	srand(time(NULL));
+	const int N = 1;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+		nosort[i] = rand() % 1000;
+
+	insertion_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	for (int i = 0; i < N; i++)
+	{
+		REQUIRE(nosort[i] == sorted[i]);
+	}
+}
+
+
+TEST_CASE("insertion_sort2", "N=10")
+{
+	srand(time(NULL));
+	const int N = 10;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = rand() % 1000;
+	}
+
+	insertion_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	for (int i = 0; i < N; i++)
+	{
+		REQUIRE(nosort[i] == sorted[i]);
+	}
+}
+
+
+TEST_CASE("insertion_sort3", "N = 100")
+{
+	srand(time(NULL));
+	const int N = 100;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = rand() % 1000;
+	}
+
+	insertion_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	for (int i = 0; i < N; i++)
+	{
+		REQUIRE(nosort[i] == sorted[i]);
+	}
+}
+
+
+TEST_CASE("insertion_sort4", "N=500")
+{
+	srand(time(NULL));
+	const int N = 500;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = rand() % 1000;
+	}
+
+	insertion_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	for (int i = 0; i < N; i++)
+	{
+		REQUIRE(nosort[i] == sorted[i]);
+	}
+}
+
+
+TEST_CASE("insertion_sort5", "N=1000")
+{
+	srand(time(NULL));
+	const int N = 1000;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = rand() % 1000;
+	}
+
+	insertion_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	for (int i = 0; i < N; i++)
+	{
+		REQUIRE(nosort[i] == sorted[i]);
+	}
+}
+
+
+//////////////////////////////////// Сортировка обратных массивов длины 10, 100, 500 и 1000 ////////////////////////////////////
+
+
+TEST_CASE("insertion_sort_swap_1", "N=10")
+{
+	srand(time(NULL));
+	const int N = 10;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = N - i;
+	}
+
+	insertion_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	for (int i = 0; i < N; i++)
+	{
+		REQUIRE(nosort[i] == sorted[i]);
+	}
+}
+
+TEST_CASE("insertion_sort_swap_2", "N=100")
+{
+	srand(time(NULL));
+	const int N = 100;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = N - i;
+	}
+
+	insertion_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	for (int i = 0; i < N; i++)
+	{
+		REQUIRE(nosort[i] == sorted[i]);
+	}
+}
+
+TEST_CASE("insertion_sort_swap_3", "N=500")
+{
+	srand(time(NULL));
+	const int N = 500;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = N - i;
+	}
+
+	insertion_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	for (int i = 0; i < N; i++)
+	{
+		REQUIRE(nosort[i] == sorted[i]);
+	}
+}
+
+TEST_CASE("insertion_sort_swap_4", "N=1000")
+{
+	srand(time(NULL));
+	const int N = 1000;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = N - i;
+	}
+
+	insertion_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	for (int i = 0; i < N; i++)
+	{
+		REQUIRE(nosort[i] == sorted[i]);
+	}
+}
+
+
+//////////////////////////////////// Сортировка массивов, заполненных значениями в промежутке [0; 3] ////////////////////////////////////
+
+
+TEST_CASE("insertion_sort_fewunique", "N=1")
+{
+	srand(time(NULL));
+	const int N = 1;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = rand() % 3;
+	}
+
+	insertion_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	for (int i = 0; i < N; i++)
+	{
+		REQUIRE(nosort[i] == sorted[i]);
+	}
+}
+
+TEST_CASE("insertion_sort_fewunique_1", "N=10")
+{
+	srand(time(NULL));
+	const int N = 10;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = rand() % 3;
+	}
+
+	insertion_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	for (int i = 0; i < N; i++)
+	{
+		REQUIRE(nosort[i] == sorted[i]);
+	}
+}
+
+
+TEST_CASE("insertion_sort_fewunique_2", "N=100")
+{
+	srand(time(NULL));
+	const int N = 100;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = rand() % 3;
+	}
+
+	insertion_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	for (int i = 0; i < N; i++)
+	{
+		REQUIRE(nosort[i] == sorted[i]);
+	}
+}
+
+
+TEST_CASE("insertion_sort_fewunique_3", "N=500")
+{
+	srand(time(NULL));
+	const int N = 500;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = rand() % 3;
+	}
+
+	insertion_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	for (int i = 0; i < N; i++)
+	{
+		REQUIRE(nosort[i] == sorted[i]);
+	}
+}
+
+
+TEST_CASE("insertion_sort_fewunique_4", "N=1000")
+{
+	srand(time(NULL));
+	const int N = 1000;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = rand() % 3;
+	}
+
+	insertion_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	for (int i = 0; i < N; i++)
+	{
+		REQUIRE(nosort[i] == sorted[i]);
+	}
+}
