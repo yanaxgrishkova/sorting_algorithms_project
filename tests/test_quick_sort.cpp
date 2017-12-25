338ed05df52de378cc4a9e8ed698f4707defbe0c
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-12-25 10:27:02 +0300

    Create test_quick_sort.cpp

---
+++ tests/test_quick_sort.cpp
@@ -0,0 +1,364 @@
+#include <catch.hpp>
+#include "quick_sort.h"
+#include <ctime>
+//////////////////////////////////// Сортировка рандомных массивов длины 1, 10, 100, 500 и 1000 ////////////////////////////////////
+
+TEST_CASE("quick_sort", "N=1")
+{
+	srand(time(NULL));
+	const int N = 1;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+		nosort[i] = rand() % 1000;
+
+	quick_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	bool flag = true;
+	for (int i = 0; i < N; i++)
+	{
+		if (nosort[i] != sorted[i])
+			flag = false;
+	}
+	REQUIRE(flag == true);
+}
+
+
+TEST_CASE("quick_sort2", "N=10")
+{
+	srand(time(NULL));
+	const int N = 10;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = rand() % 1000;
+	}
+
+	quick_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	bool flag = true;
+	for (int i = 0; i < N; i++)
+	{
+		if (nosort[i] != sorted[i])
+			flag = false;
+	}
+	REQUIRE(flag == true);
+}
+
+
+TEST_CASE("quick_sort3", "N = 100")
+{
+	srand(time(NULL));
+	const int N = 100;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = rand() % 1000;
+	}
+
+	quick_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	bool flag = true;
+	for (int i = 0; i < N; i++)
+	{
+		if (nosort[i] != sorted[i])
+			flag = false;
+	}
+	REQUIRE(flag == true);
+}
+
+
+TEST_CASE("quick_sort4", "N=500")
+{
+	srand(time(NULL));
+	const int N = 500;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = rand() % 1000;
+	}
+
+	quick_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	bool flag = true;
+	for (int i = 0; i < N; i++)
+	{
+		if (nosort[i] != sorted[i])
+			flag = false;
+	}
+	REQUIRE(flag == true);
+}
+
+
+TEST_CASE("quick_sort5", "N=1000")
+{
+	srand(time(NULL));
+	const int N = 1000;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = rand() % 1000;
+	}
+
+	quick_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+	bool flag = true;
+	for (int i = 0; i < N; i++)
+	{
+		if (nosort[i] != sorted[i])
+			flag = false;
+	}
+	REQUIRE(flag == true);
+}
+
+
+//////////////////////////////////// Сортировка обратных массивов длины 10, 100, 500 и 1000 ////////////////////////////////////
+
+
+TEST_CASE("quick_sort_swap_1", "N=10")
+{
+	srand(time(NULL));
+	const int N = 10;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = N - i;
+	}
+
+	quick_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	bool flag = true;
+	for (int i = 0; i < N; i++)
+	{
+		if (nosort[i] != sorted[i])
+			flag = false;
+	}
+	REQUIRE(flag == true);
+}
+
+TEST_CASE("quick_sort_swap_2", "N=100")
+{
+	srand(time(NULL));
+	const int N = 100;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = N - i;
+	}
+
+	quick_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	bool flag = true;
+	for (int i = 0; i < N; i++)
+	{
+		if (nosort[i] != sorted[i])
+			flag = false;
+	}
+	REQUIRE(flag == true);
+}
+
+TEST_CASE("quick_sort_swap_3", "N=500")
+{
+	srand(time(NULL));
+	const int N = 500;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = N - i;
+	}
+
+	quick_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	bool flag = true;
+	for (int i = 0; i < N; i++)
+	{
+		if (nosort[i] != sorted[i])
+			flag = false;
+	}
+	REQUIRE(flag == true);
+}
+
+TEST_CASE("quick_sort_swap_4", "N=1000")
+{
+	srand(time(NULL));
+	const int N = 1000;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = N - i;
+	}
+
+	quick_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	bool flag = true;
+	for (int i = 0; i < N; i++)
+	{
+		if (nosort[i] != sorted[i])
+			flag = false;
+	}
+	REQUIRE(flag == true);
+}
+
+
+//////////////////////////////////// Сортировка массивов, заполненных значениями в промежутке [0; 3] ////////////////////////////////////
+
+
+TEST_CASE("quick_sort_fewunique", "N=1")
+{
+	srand(time(NULL));
+	const int N = 1;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = rand() % 3;
+	}
+
+	quick_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	bool flag = true;
+	for (int i = 0; i < N; i++)
+	{
+		if (nosort[i] != sorted[i])
+			flag = false;
+	}
+	REQUIRE(flag == true);
+}
+
+TEST_CASE("quick_sort_fewunique_1", "N=10")
+{
+	srand(time(NULL));
+	const int N = 10;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = rand() % 3;
+	}
+
+	quick_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	bool flag = true;
+	for (int i = 0; i < N; i++)
+	{
+		if (nosort[i] != sorted[i])
+			flag = false;
+	}
+	REQUIRE(flag == true);
+}
+
+
+TEST_CASE("quick_sort_fewunique_2", "N=100")
+{
+	srand(time(NULL));
+	const int N = 100;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = rand() % 3;
+	}
+
+	quick_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	bool flag = true;
+	for (int i = 0; i < N; i++)
+	{
+		if (nosort[i] != sorted[i])
+			flag = false;
+	}
+	REQUIRE(flag == true);
+}
+
+TEST_CASE("quick_sort_fewunique_3", "N=500")
+{
+	srand(time(NULL));
+	const int N = 500;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = rand() % 3;
+	}
+
+	quick_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	bool flag = true;
+	for (int i = 0; i < N; i++)
+	{
+		if (nosort[i] != sorted[i])
+			flag = false;
+	}
+	REQUIRE(flag == true);
+}
+
+TEST_CASE("quick_sort_fewunique_4", "N=1000")
+{
+	srand(time(NULL));
+	const int N = 1000;
+
+	int nosort[N];
+	for (int i = 0; i<N; i++)
+	{
+		nosort[i] = rand() % 3;
+	}
+
+	quick_sort(&nosort[0], &nosort[N]);
+
+	std::vector<int> sorted(nosort, nosort + N);
+	std::sort(sorted.begin(), sorted.end());
+
+	bool flag = true;
+	for (int i = 0; i < N; i++)
+	{
+		if (nosort[i] != sorted[i])
+			flag = false;
+	}
+	REQUIRE(flag == true);
+}
