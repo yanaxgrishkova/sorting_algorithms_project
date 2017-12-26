7785130e2e3d2175bdd5290c814977849bbbc88f
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-12-26 15:37:56 +0300

    Update selection_sort.h

--- include/selection_sort.h
+++ include/selection_sort.h
@@ -7,7 +7,9 @@ void selection_sort(ForwardIterator first, ForwardIterator last)
 	for (auto i = first; i != last; ++i)
 	{
 		ForwardIterator min = i;
-		for (auto it = i, std::advance(it, 1); it != last; ++it)
+		auto it = i;
+		std::advance(it, 1);
+		for (; it != last; ++it)
 		{
 			if (*it < *min)
 				std::iter_swap(it, min);
