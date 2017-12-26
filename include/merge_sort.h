3d12644c37e24cd819d0bd5c758ead5c493b2e09
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-12-26 06:00:03 +0300

    Update merge_sort.h

--- include/merge_sort.h
+++ include/merge_sort.h
@@ -2,45 +2,50 @@
 #include <ctime> 
 #include <utility>
 
-template <typename Iterator, typename LessThan>
-void merge_sort(Iterator first, Iterator last, LessThan lessThan, size_t size = 0)
+template <typename  RandomAccessIterator, typename FindMin>
+void merge_sort(RandomAccessIterator first, RandomAccessIterator last, FindMin min, size_t size = 0)
 {
 	if (size == 0 && first != last)
 		size = std::distance(first, last);
 
 	if (size <= 1)
 		return;
-	
+
 	size_t first_half = size / 2;
 	size_t second_half = size - first_half;
-	Iterator middle = first;
+	RandomAccessIterator middle = first;
 	std::advance(middle, first_half);
-	
-	merge_sort(first, middle, lessThan, first_half);
-	merge_sort(middle, last, lessThan, second_half);
-	
-	Iterator right = middle;
+
+	merge_sort(first, middle, min, first_half);
+	merge_sort(middle, last, min, second_half);
+
+	RandomAccessIterator right = middle;
+
 	while (first != middle)
 	{
-		if (lessThan(*right, *first))
+		if (min(*right, *first))
 		{
-			typename std::iterator_traits<Iterator>::value_type misplaced =
+			typename std::iterator_traits<RandomAccessIterator>::value_type replace =
 				std::move(*first);
 			*first = std::move(*right);
-			Iterator scan = right;
-			Iterator next = scan;
+			RandomAccessIterator scan = right;
+			RandomAccessIterator next = scan;
 			++next;
-			while (next != last && lessThan(*next, misplaced))
+
+			while (next != last && min(*next, replace))
+			{
 				*scan++ = std::move(*next++);
-			*scan = std::move(misplaced);
+			}
+
+			*scan = std::move(replace);
 		}
 		++first;
 	}
 }
 
-template <typename Iterator>
-void merge_sort(Iterator first, Iterator last)
+template <typename RandomAccessIterator>
+void merge_sort(RandomAccessIterator first, RandomAccessIterator last)
 {
 	merge_sort(first, last,
-		std::less<typename std::iterator_traits<Iterator>::value_type>());
+		std::less<typename std::iterator_traits<RandomAccessIterator>::value_type>());
 }
